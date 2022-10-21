e993c918f3a6fa246edb44854d6b59943e345dd4
ReplayCoding <replaycoding@gmail.com>
2022-10-21 11:58:12 -0700

    remove useless console.log

--- src/solution.ts
+++ src/solution.ts
@@ -5,7 +5,6 @@ function day1_p1() {
   let buf = readFileSync('inputs/day1_p1.txt');
   let data = buf.toString().split("\n").map(x => parseInt(x.trim()));
 
-  console.log(data);
   console.log(countIncreases(data));
 }
 
