9fe5b0207daf524f44a3baf5e045fbf9bc150195
ReplayCoding <replaycoding@gmail.com>
2022-10-21 12:32:20 -0700

    add readLinesFromFile function for solution

--- src/solution.ts
+++ src/solution.ts
@@ -2,17 +2,18 @@ import { countIncreases } from './count_increases';
 import { countSlidingWindowIncreases } from './sliding_window';
 import { readFileSync } from 'fs';
 
-function day1_p1() {
-  let buf = readFileSync('inputs/day1.txt');
-  let data = buf.toString().split("\n").map(x => parseInt(x.trim()));
+function readLinesFromFile(fname: string): string[] {
+  let buf = readFileSync(fname);
+  return buf.toString().split("\n")
+}
 
+function day1_p1() {
+  let data = readLinesFromFile('inputs/day1.txt').map(x => parseInt(x.trim()));
   console.log(countIncreases(data));
 }
 
 function day1_p2() {
-  let buf = readFileSync('inputs/day1.txt');
-  let data = buf.toString().split("\n").map(x => parseInt(x.trim()));
-
+  let data = readLinesFromFile('inputs/day1.txt').map(x => parseInt(x.trim()));
   console.log(countSlidingWindowIncreases(data));
 }
 
