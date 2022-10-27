5345f1a4d5aca0c6fc32dc4f4f88660cf1b79e46
ReplayCoding <replaycoding@gmail.com>
2022-10-27 13:53:11 -0700

    add day9 solution

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,10 +1,11 @@
 import { countIncreases } from './count_increases';
 import { countSlidingWindowIncreases } from './sliding_window';
+import { getRiskLevelOfGrid } from './low_points';
 import { readFileSync } from 'fs';
 
 function readLinesFromFile(fname: string): string[] {
   let buf = readFileSync(fname);
-  return buf.toString().split("\n")
+  return buf.toString().trimEnd().split("\n")
 }
 
 function day1_p1() {
@@ -17,8 +18,16 @@ function day1_p2() {
   console.log(countSlidingWindowIncreases(data));
 }
 
+function day9_p1() {
+  let data = readLinesFromFile('inputs/day9.txt').map(line => line.split("").map(Number))
+  console.log(getRiskLevelOfGrid(data));
+}
+
 console.log("Day 1 Part 1 solution:")
 day1_p1();
 
 console.log("Day 1 Part 2 solution:")
 day1_p2();
+
+console.log("Day 9 Part 1 solution:")
+day9_p1();
